27c5003eb5d4dd23556912cf3978a2a901b26fb4
David Barcenas <dbarcenas.dev@gmail.com>
2023-03-08 18:18:56 -0600

    create logo design

---
+++ src/components/logo.tsx
@@ -0,0 +1,27 @@
+export function Logo() {
+  return (
+    <div
+      className='
+            rounded-full 
+            bg-indigo-400 
+            w-6 
+            h-6 
+            relative 
+            before:absolute 
+            before:block 
+            before:w-full 
+            before:h-full 
+            before:left-4 
+            before:rounded-full 
+            before:bg-indigo-500
+            after:absolute 
+            after:block 
+            after:w-full 
+            after:h-full 
+            after:left-8 
+            after:rounded-full 
+            after:bg-indigo-600
+            '
+    ></div>
+  );
+}
