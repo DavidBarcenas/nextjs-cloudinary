b7e29e750bfec91a9a049fd9ad2205c961e5698e
David Barcenas <dbarcenas.dev@gmail.com>
2023-03-11 22:12:59 -0600

    add constants file

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+export const errorMessages = {
+  unparsedImage: 'Algo salió mal. No se pudo convertir la imagen a base64',
+  methodNotAllowed: 'El método de petición no es válido para esta URL',
+  fileInBody: 'La propiedad "File" es requerida.',
+  serverError: 'Lo sentimos, algo salió mal. Por favor, inténtelo de nuevo más tarde',
+  requestFailure:
+    'Lo sentimos, no se pudo completar tu solicitud. Por favor, inténtelo de nuevo más tarde',
+};
