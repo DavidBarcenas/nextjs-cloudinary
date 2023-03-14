a78620eb92d22c31023be89743508deb207448f1
David Barcenas <dbarcenas.dev@gmail.com>
2023-03-14 11:35:16 -0600

    fix button animation

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ export function Button({ children, className }: Props) {
     <button
       className={`${
         className || ''
-      } relative py-2 px-5 rounded-lg border border-[#3d6aff] text-sm uppercase font-semibold tracking-wider bg-transparent text-white overflow-hidden shadow shadow-transparent transition duration-300 ease-in hover:bg-[#3d6aff] hover:shadow-3xl hover:ease-out hover:before:animate-shine before:block before:w-0 before:h-[86%] before:absolute before:top-[7%] before:l-0 before:opacity-0 before:bg-white before:shadow-4xl before:-skew-x-[-20ded]`}
+      } relative py-2 px-5 rounded-lg border border-[#3d6aff] text-sm uppercase font-semibold tracking-wider bg-[#3d6aff] text-white overflow-hidden shadow shadow-transparent transition duration-200 ease-in hover:bg-[#3d6aff] hover:shadow-3xl hover:ease-out hover:before:animate-shine before:block before:w-0 before:h-[86%] before:absolute before:top-[7%] before:left-0 before:opacity-0 before:bg-white before:shadow-4xl before:-skew-x-[-20deg] active:shadow-transparent active:transition-shadow active:ease-in`}
     >
       {children}
     </button>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,16 @@
+import { Button } from './button';
 import { DownloadIcon } from './icons';
 import { Logo } from './logo';
 
 export function Header() {
   return (
-    <header className='flex items-center justify-between px-4 leading-[60px] border-b border-gray-700'>
+    <header className='flex items-center justify-between py-4 px-5 leading-[60px] border-b border-gray-700'>
       <Logo />
       <nav className='flex items-center'>
-        <a href='' className='flex items-center'>
+        <Button className='flex items-center'>
           <DownloadIcon />
           <span className='ml-2'>Descargar</span>
-        </a>
+        </Button>
       </nav>
     </header>
   );
