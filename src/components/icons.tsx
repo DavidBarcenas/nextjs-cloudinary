502d3a22bbb1645030af890c4ad1f4acb3c1e63b
David Barcenas <dbarcenas.dev@gmail.com>
2023-03-14 22:37:47 -0600

    add icons to upload image

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -28,3 +28,39 @@ export const DownloadIcon = () => (
     <path d='M9 19l3 3l3 -3'></path>
   </svg>
 );
+
+export const UploadIcon = ({ size = '24' }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={size}
+    height={size}
+    viewBox='0 0 24 24'
+    strokeWidth='1'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+    <path d='M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7h-1'></path>
+    <path d='M9 15l3 -3l3 3'></path>
+    <path d='M12 12l0 9'></path>
+  </svg>
+);
+
+export const ChevronDown = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='16'
+    height='16'
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+    <path d='M6 9l6 6l6 -6'></path>
+  </svg>
+);
