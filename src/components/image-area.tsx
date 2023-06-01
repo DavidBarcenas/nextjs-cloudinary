c11a63675e0ea3b7f3749a6036f96b7ff389bb5c
David Barcenas <dbarcenas.dev@gmail.com>
2023-05-31 23:17:07 -0600

    show dropzone

--- src/components/image-area.tsx
+++ src/components/image-area.tsx
@@ -2,13 +2,11 @@ import { Dropzone } from '@/components/dropzone';
 import { Loader } from '@/components/loader';
 import { useRequestStatusStore } from '@/stores/request-status';
 import { ImagePreview } from '@/components/image-preview';
-import { CompareImages } from '@/components/compare-images';
 
 export function ImageArea() {
   const status = useRequestStatusStore((state) => state.status);
 
   if (status === 'loading') return <Loader />;
   if (status === 'success') return <ImagePreview />;
-  // return <Dropzone />;
-  return <CompareImages />;
+  return <Dropzone />;
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -60,10 +60,10 @@ export function Sidebar() {
 
   return (
     <aside className='w-full h-full max-w-xs border-l border-gray-700 p-6 flex flex-col'>
-      <div className='flex items-center justify-between'>
+      <div className='flex items-center justify-between mb-6'>
         <span>Comparar</span>
-        <label className='relative inline-flex items-center cursor-pointer'>
-          <input type='checkbox' value='' className='sr-only peer' />
+        <label htmlFor='compare' className='relative inline-flex items-center cursor-pointer'>
+          <input id='compare' type='checkbox' className='sr-only peer' />
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
         </label>
       </div>
